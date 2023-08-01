ce309162a6499f629827118a100e4223b315be3e
Matt Fowler <mbfowler@gmail.com>
2023-08-01 14:19:26 -0400

    Add App component outline and imports

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,25 @@
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
+import Nav from '../Nav/Nav';
+import Home from '../Home/Home';
+import Saved from '../Saved/Saved';
+import Settings from '../Settings/Settings';
 
 const App = () => {
 
   
   return (
-    <div className="App">
-
-    </div>
+    <>
+      <Nav />
+      <main className="App">
+        <Routes>
+          <Route path='/' element={<Home />}/>
+          <Route path='/saved' element={<Saved />}/>
+          <Route path='/settings' element={<Settings />}/>
+        </Routes>
+      </main>
+    </>
+    
   );
 }
 
