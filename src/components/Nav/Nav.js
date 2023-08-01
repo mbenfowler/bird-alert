6c21715a24237d1aa5c49cf1870f82b33e7c9580
Matt Fowler <mbfowler@gmail.com>
2023-08-01 15:22:23 -0400

    Add initial styling and fonts for Nav

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,6 +1,15 @@
+import { Link } from "react-router-dom"
+import './Nav.css'
+
 const Nav = () => {
     return (
-        <div></div>
+        <nav>
+            <Link className='text-link' to='/'><span id='titleBird'>Bird </span><span id='titleAlert'>Alert!</span></Link>
+            <div>
+                <Link to='/saved'><img className='nav-img' src={process.env.PUBLIC_URL + '/images/bird.png'} alt='saved birds'/></Link>
+                <Link to='/settings'><img className='nav-img' src={process.env.PUBLIC_URL + '/images/settings.png'} alt='user settings'/></Link>
+            </div>
+        </nav>
     )
 }
 
