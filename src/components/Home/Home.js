069ed5f82b2b0d75855b3a80582be885702b3a3d
Matt Fowler <mbfowler@gmail.com>
2023-08-03 18:27:09 -0400

    Remove unneeded import

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,4 +1,4 @@
-import { useState, useEffect, useContext } from "react"
+import { useContext } from "react"
 import BirdsList from "../BirdsList/BirdsList"
 import BirdsContext from "../BirdsContext/BirdsContext"
 import './Home.css'
