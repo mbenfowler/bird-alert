02275ca834260306d8c6a4bc9ad0c297ce183a74
mbfowler <mbfowler@gmail.com>
2023-11-05 11:14:01 -0500

    Add knex db and init migration

---
+++ db/seeds/dev/users.js
@@ -0,0 +1,44 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> } 
+ */
+exports.seed = async function(knex) {
+  // Deletes ALL existing entries
+  try {
+    await knex('savedBirds').del()
+    await knex('birds').del()
+    await knex('users').del()
+
+    const [userID] = await knex('users').insert({
+      username: '',
+      password: '',
+      email: 'robinware456@gmail.com',
+      phone: '555-555-5555',
+      location: 'US-GA-139',
+      state: ''
+    }, 'id')
+
+    const [birdID] = await knex('birds').insert({
+      comName: 'Snow Goose',
+      sciName: 'Anser caerulescens',
+      speciesCode: 'snogoo',
+      category: 'species',
+      order: 'Anseriformes',
+      familyCode: 'anatid1',
+      familyComName: 'Ducks, Geese, and Waterfowl',
+      familySciName: 'Anatidae',
+      wikiURL: 'https://en.wikipedia.org/?curid=199013',
+      taxonOrder: 256
+    }, 'id')
+
+    console.log('userID: ', userID)
+    console.log('birdID: ', birdID)
+
+    return knex('savedBirds').insert({
+      user_id: userID.id,
+      bird_id: birdID.id
+    })
+  } catch (error) {
+    console.log(`Error seeding data: ${error}`)
+  }
+};
--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require('express')
 const cors = require('cors')
 const app = express()
 
+const environment = process.env.NODE_ENV || 'development'
+const configuration = require('./knexfile')[environment]
+const db = require('knex')(configuration)
+
 app.set('port', process.env.PORT || 3001)
 app.locals.title = 'Bird Alert'
 
