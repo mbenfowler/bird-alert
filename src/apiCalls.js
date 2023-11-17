54791050123f8b279dd776fa104e2294b59e98fc
mbfowler <mbfowler@gmail.com>
2023-11-17 12:09:38 -0500

    Append small image url

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -23,7 +23,7 @@ const getBirdsData = async (keys) => {
     for (const key of keys) {
         const birdData = await getBirdData(key)
         const nuthatchData = await getBirdImg(birdData[0].comName)
-        const birdImg = nuthatchData.entities[0]?.images[0]
+        const birdImg = `${nuthatchData.entities[0]?.images[0]}?q=75&fm=jpg&w=400&fit=max`
         const wikiData = await getBirdWiki(birdData[0].comName)
         const wikiURL = `https://en.wikipedia.org/?curid=${wikiData.pages[0].id}`
         birdsData.push({...birdData[0], birdImg, wikiURL})
