208fcd86ed7915fa8d65c0ff1e2729160a1cbf08
mbfowler <mbfowler@gmail.com>
2023-11-24 18:07:26 -0500

    Add nanoid for AlertCard keys

--- src/components/BirdAlerts/BirdAlerts.js
+++ src/components/BirdAlerts/BirdAlerts.js
@@ -1,11 +1,12 @@
 import { Link } from "react-router-dom"
+import { nanoid } from 'nanoid'
 import AlertCard from "../AlertCard/AlertCard"
 import './BirdAlerts.css'
 
 const BirdAlerts = ({ alerts }) => {
     const birdAlerts = alerts.map(alert => {
       return (
-        <Link key={alert.subId} to={`https://www.google.com/maps?q=${alert.lat},${alert.lng}&label=${alert.comName}`} target="_blank">
+        <Link key={nanoid()} to={`https://www.google.com/maps?q=${alert.lat},${alert.lng}&label=${alert.comName}`} target="_blank">
           <AlertCard alert={alert} />
         </Link>
       )
