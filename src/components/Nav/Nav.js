a6319621b9a7b20a51ea1875de3890efce2e9828
mbfowler <mbfowler@gmail.com>
2023-11-26 11:49:28 -0500

    Try different image

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -11,7 +11,7 @@ const Nav = ({setNetworkError}) => {
         <nav>
             <Link onClick={handleClick} className='text-link' to='/'><span id='titleBird'>Bird </span><span id='titleAlert'>Alert!</span></Link>
             <div>
-                <Link to='/saved'><img className='nav-img' src={process.env.PUBLIC_URL + '/images/bird.png'} alt='saved birds'/></Link>
+                <Link to='/saved'><img className='nav-img' src={process.env.PUBLIC_URL + '/images/bird.ico'} alt='saved birds'/></Link>
                 <Link to='/settings'><img className='nav-img' src={process.env.PUBLIC_URL + '/images/settings.png'} alt='user settings'/></Link>
             </div>
         </nav>
